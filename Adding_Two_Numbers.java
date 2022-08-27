8aeb8e892c8418b5789382a33255b1180013b2f5
21A91A05D9 <97424618+21A91A05D9@users.noreply.github.com>
2022-08-27 13:54:33 +0530

    import java.util.Scanner; import java.lang.Math; class cd {     public static void main(String[] args)     {         Scanner in = new Scanner(System.in);         int  a=in.nextInt();         int  b=in.nextInt();         int d=a+b;         System.out.println(d);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.lang.Math;
+class cd
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int  a=in.nextInt();
+        int  b=in.nextInt();
+        int d=a+b;
+        System.out.println(d);
+    }
+}
