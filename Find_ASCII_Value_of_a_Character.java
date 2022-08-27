1def5ff00d45543bb207ff5e0debe97692d39d17
21A91A05D9 <97424618+21A91A05D9@users.noreply.github.com>
2022-08-27 14:13:21 +0530

    import java.util.Scanner; import java.lang.Math; class cd {     public static void main(String[] args)     {         Scanner in = new Scanner(System.in);         char a=in.next().charAt(0);         int d=(int)a;         System.out.println(d);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.lang.Math;
+class cd
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        char a=in.next().charAt(0);
+        int d=(int)a;
+        System.out.println(d);
+    }
+}
